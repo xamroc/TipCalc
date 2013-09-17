de87175b1dd276ef4eb1669adba22faa427de0d1
Marco Lau <lau.career@gmail.com>
2013-09-17 15:11:55 +0800

    Setup basic GUI interface: bill, tip, and final bill

--- gen/com/example/tipcalc/R.java
+++ gen/com/example/tipcalc/R.java
@@ -24,8 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int textView1=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int billEditText=0x7f080000;
+        public static final int billTextView=0x7f080001;
+        public static final int finalBillEditText=0x7f080005;
+        public static final int finalBillTextView=0x7f080004;
+        public static final int tipEditText=0x7f080003;
+        public static final int tipTextView=0x7f080002;
     }
     public static final class layout {
         public static final int activity_tip_calc=0x7f030000;
@@ -36,6 +41,13 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int bill_edit_text=0x7f050003;
+        public static final int bill_text_view=0x7f050002;
+        public static final int change_tip_text_view=0x7f050008;
+        public static final int final_bill_edit_text=0x7f050007;
+        public static final int final_bill_text_view=0x7f050006;
+        public static final int tip_edit_text=0x7f050005;
+        public static final int tip_text_view=0x7f050004;
     }
     public static final class style {
         /** 
