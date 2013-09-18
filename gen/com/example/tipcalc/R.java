3b996d86f9d8edbe78db23e5e9bc44b97b3c735c
Marco Lau <lau.career@gmail.com>
2013-09-18 23:54:50 +0800

    Update strings.xml for service review functionality

--- gen/com/example/tipcalc/R.java
+++ gen/com/example/tipcalc/R.java
@@ -8,6 +8,9 @@
 package com.example.tipcalc;
 
 public final class R {
+    public static final class array {
+        public static final int problem-solving=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -24,30 +27,42 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
-        public static final int billEditText=0x7f080000;
-        public static final int billTextView=0x7f080001;
-        public static final int changeTipSeekBar=0x7f080007;
-        public static final int changeTipTextView=0x7f080006;
-        public static final int finalBillEditText=0x7f080005;
-        public static final int finalBillTextView=0x7f080004;
-        public static final int tipEditText=0x7f080003;
-        public static final int tipTextView=0x7f080002;
+        public static final int action_settings=0x7f090008;
+        public static final int billEditText=0x7f090000;
+        public static final int billTextView=0x7f090001;
+        public static final int changeTipSeekBar=0x7f090007;
+        public static final int changeTipTextView=0x7f090006;
+        public static final int finalBillEditText=0x7f090005;
+        public static final int finalBillTextView=0x7f090004;
+        public static final int tipEditText=0x7f090003;
+        public static final int tipTextView=0x7f090002;
     }
     public static final class layout {
         public static final int activity_tip_calc=0x7f030000;
     }
     public static final class menu {
-        public static final int tip_calc=0x7f070000;
+        public static final int tip_calc=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int available_bad_radiobutton=0x7f05000e;
+        public static final int available_good_radiobutton=0x7f050010;
+        public static final int available_ok_radiobutton=0x7f05000f;
+        public static final int available_text_view=0x7f05000d;
         public static final int bill_edit_text=0x7f050003;
         public static final int bill_text_view=0x7f050002;
         public static final int change_tip_text_view=0x7f050008;
         public static final int final_bill_edit_text=0x7f050007;
         public static final int final_bill_text_view=0x7f050006;
+        public static final int intro_friendly_text_view=0x7f05000a;
+        public static final int intro_opinion_text_view=0x7f05000c;
+        public static final int intro_specials_text_view=0x7f05000b;
+        public static final int intro_text_view=0x7f050009;
+        public static final int pause_chronometer_button=0x7f050013;
+        public static final int reset_chronometer_button=0x7f050014;
+        public static final int start_chronometer_button=0x7f050012;
+        public static final int time_waiting_text_view=0x7f050011;
         public static final int tip_edit_text=0x7f050005;
         public static final int tip_text_view=0x7f050004;
     }
@@ -73,10 +88,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
